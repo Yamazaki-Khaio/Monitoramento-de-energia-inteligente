e4f6473be5cbda35ca319bb5d22be955cae40a10
Yamazaki Khaio <91100781+Yamazaki-Khaio@users.noreply.github.com>
2023-03-14 23:24:24 -0300

    Add files via upload

---
+++ pythonProject1/models/medidor.py
@@ -0,0 +1,99 @@
+from datetime import datetime
+import json
+import random
+import socket
+import time
+import uuid
+import requests
+import argparse
+
+from servidor import write_database, read_database
+
+IP = "0.0.0.0"
+PORTA = 5900
+
+# ID exclusivo do cliente
+client_id = uuid.uuid1()
+
+
+url = f"http://localhost:5000/{client_id}"
+http_request = f"POST / HTTP/1.1\r\nHost: {IP}:{PORTA}\r\n\r\n".encode()
+
+print(f"O id do Cliente é: {client_id}, para acessar os dados do medidor basta acessar: http://0.0.0.0:5000/?id={client_id}")
+
+# Configurar a leitura dos argumentos da linha de comando
+parser = argparse.ArgumentParser(description='Monitor de consumo de energia elétrica')
+parser.add_argument('--limite-alerta', type=int, default=500, help='Limite de alerta em kWh')
+parser.add_argument('--multiplicador', type=float, default=1.0, help='Fator de multiplicação do consumo em kWh')
+args = parser.parse_args()
+
+# Verificar se o client_id já existe no arquivo JSON
+storage_data = read_database()
+if client_id in storage_data:
+    # Carrega o histórico de consumo a partir do arquivo JSON
+    historico_consumo = storage_data[client_id]['historico']
+else:
+    # Cria uma lista vazia para o histórico de consumo
+    historico_consumo = []
+# Executar o monitor de energia com os argumentos fornecidos
+limite_alerta = args.limite_alerta
+multiplicador = args.multiplicador
+while True:
+    consumo_atual = random.randint(100, 1000)
+    data_hora_atual = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    consumo_kwh = consumo_atual * multiplicador
+    consumo_total = consumo_atual + consumo_kwh
+    fatura = consumo_total * 0.5  # R$ 0,50 por kWh
+
+    # Adiciona o consumo atual ao histórico de consumo
+    historico_consumo.append({
+        'data_hora': data_hora_atual,
+        'consumo_kwh': consumo_kwh
+    })
+
+    # Verificar se o consumo está acima do limite de alerta
+    if consumo_atual > limite_alerta:
+       alerta = f"{data_hora_atual} - ALERTA: Consumo muito alto: {consumo_kwh:.2f} kWh"
+    else:
+        alerta = ""
+
+
+    # Define o payload com os dados a serem enviados
+    payload = {
+        'consumo_atual_kWh': consumo_atual,
+        'consumo_kwh': consumo_kwh,
+        'consumo_total_kWh': consumo_total,
+        'alerta': alerta,
+        'historico_consumo_kWh': historico_consumo,
+        'fatura_a_pagar': fatura
+    }
+
+    # Converte o payload em uma string JSON
+    message = json.dumps(payload)
+    # Cria o socket
+  
+    # Envia a mensagem para o servidor
+    response = requests.post(url, json=payload)
+
+    storage_data = read_database()
+    # Verifica se o client_id já existe no arquivo JSON
+    if client_id in storage_data:
+        # Adiciona as informações do medidor às informações existentes do cliente
+        storage_data[str(client_id)]['medidores'].append(payload)
+    else:
+        # Cria um novo registro para o cliente com as informações do medidor
+        storage_data[str(client_id)] = {
+            'medidores': [payload]
+        }
+
+    # Salva as informações atualizadas no arquivo JSON
+    write_database(storage_data)
+
+    # Incrementa o valor atual de energia medido
+
+
+    #valor_atual += incremento
+
+    # Aguarda um segundo antes de medir novamente
+    time.sleep(1)
+
---
+++ pythonProject1/models/servidor.py
@@ -0,0 +1,271 @@
+import socket
+import sys
+from threading import Thread
+import json
+import os
+HOST = "0.0.0.0"
+PORT = 5000
+MAX_BUFFER_SIZE = 1024
+database = {}
+body = None
+
+def main():
+    initialize_database()
+    start_server()
+
+
+# Criar arquivo json
+def initialize_database():
+    # Create database file
+    if not os.path.exists("db.json"):
+        with open("db.json", "w+") as db:
+            db.write("{\n}")
+
+
+# Ler conteúdo do arquivo json
+def read_database():
+    try:
+        with open('db.json', 'r') as db:
+            storage_file = db.read()
+            if storage_file.strip() == "":
+                # arquivo vazio
+                return {}
+            else:
+                storage_data = json.loads(storage_file)
+                if not isinstance(storage_data, dict):
+                    # conteúdo inválido
+                    return {}
+                else:
+                    return storage_data
+    except FileNotFoundError:
+        # arquivo não encontrado
+        return {}
+
+
+# Escrever conteúdo no arquivo json
+def write_database(storage_file):
+    with open("db.json", "w") as file:
+        json.dump(storage_file, file, indent=4, sort_keys=True)
+
+
+def create_headers(status_code: int, status_text: str, message_body="") -> bytes:
+    global body
+    # headers
+    response_protocol = "HTTP/1.1"
+    response_status_code = status_code
+    response_status_text = status_text
+    response_content_type = "application/json; encoding=utf8"
+    response_connection = "close"
+    message_body_bytes = message_body.encode('utf-8') if message_body else body.encode('utf-8') if body else b''
+    response_content_length = len(message_body_bytes)
+
+    # Create seções
+    status_line = f"{response_protocol} {response_status_code} {response_status_text}\r\n"
+    connection = f"Connection: {response_connection}\r\n"
+    content_type = f"Content-Type: {response_content_type}\r\n"
+    content_length = f"Content-Length: {response_content_length}\r\n"
+    empty_line = "\r\n"
+
+    # Concatenar string
+    response_header = (
+        status_line +
+        connection +
+        content_type +
+        content_length +
+        empty_line
+    )
+
+    # Concatenar header e corpo da mensagem
+    response = response_header.encode('utf-8') + message_body_bytes
+
+    return response
+
+# Cria & start server socket
+def start_server():
+    # Cria server socket tcp
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind socket
+    try:
+        server_socket.bind((HOST, PORT))
+        print(f"Ligando o server socket ao host:{HOST} e port {PORT}")
+    except:
+        print(f"Ligação falhou. Erro: {str(sys.exc_info())}")
+        sys.exit()
+
+    # Ativando Listen passiva sockets
+    server_socket.listen(23)
+
+    while True:
+
+        # Aguardar e aceitar a conexão de entrada
+        (client_socket, address) = server_socket.accept()
+
+        ip, port = str(address[0]), str(address[1])
+        print(f"A conexão do {ip}:{port} foi estabelecida.")
+
+        try:
+            Thread(target=client_thread, args=(client_socket, ip, port)).start()
+            print(f"Client thread com o {ip}:{port} foi criado.")
+        except:
+            print(f"Client thread com {ip}:{port} não deu start.")
+
+
+
+# Thread para cada cliente
+def client_thread(client_socket, ip, port):
+    # Listen dos dados recebidos
+    response_headers = None
+    data = receive_data(client_socket)
+    print(data)
+
+    if data:
+
+        if data[0] == "GET":
+            response_headers = do_GET(data)
+
+        if data[0] == "POST":
+            response_headers = do_POST(data)
+
+        if data[0] == "PUT":
+            response_headers = do_PUT(data)
+
+        if data[0] == "DELETE":
+            response_headers = do_DELETE(data)
+
+        if response_headers is not None:
+            client_socket.send(response_headers)
+
+
+        client_socket.close()
+        print(f"Conexão do {ip}:{port} foi fechada.")
+
+    print(f"Client thread do {ip}:{port} foi fechada.")
+
+
+# Get entrada solicitada
+def get_content(data: list):
+    content = None
+
+    # procurar a entrada solicitada
+    for line in data:
+        if line.startswith("GET "):
+            content = line.split()[1]
+            break
+
+    # verificar se a entrada foi encontrada
+    if content is None:
+        return None
+
+    # verificar se a entrada está codificada em UTF-8
+    try:
+        content = content.decode('utf-8')
+    except UnicodeDecodeError:
+        return None
+
+    return content
+
+# Processo de solicitação do tipo GET
+import urllib.parse
+
+def do_GET(data: list):
+    query = urllib.parse.urlparse(data[1]).query
+    query_dict = urllib.parse.parse_qs(query)
+    name = query_dict.get('id', [''])[0]
+
+    storage_data = read_database()
+
+    if storage_data is not None:
+        if name:
+            value = storage_data.get(name)
+            if value:
+                response_body = json.dumps({name: value})
+                return create_headers(200, "OK", response_body)
+            else:
+                return create_headers(404, "Not Found")
+        else:
+            response_body = json.dumps(storage_data)
+            return create_headers(200, "OK", response_body)
+    else:
+        return create_headers(404, "Not Found")
+
+
+
+# Processo de solicitação do tipo Post
+def do_POST(data: list):
+    content = get_content(data)
+    global body
+    body = data[2]
+    if content == None:
+        return create_headers(400, "Bad Request")
+
+    storage_data = read_database()
+
+    if content in storage_data:
+        return create_headers(409, "Conflict")
+    else:
+        storage_data[content] = ""
+        write_database(storage_data)
+        return create_headers(201, "Created")
+
+# Processo de solicitação do tipo PUT
+def do_PUT(data: list):
+    content = get_content(data)
+    if content is None:
+        return create_headers(400, "Bad Request")
+
+    try:
+        content_dict = json.loads(content)
+    except json.JSONDecodeError:
+        return create_headers(400, "Invalid JSON")
+
+    if not isinstance(content_dict, dict):
+        return create_headers(400, "Invalid Data")
+
+    content_key = next(iter(content_dict), None)
+
+    if not isinstance(content_key, str):
+        return create_headers(400, "Invalid Data")
+
+    storage_data = read_database()
+
+    if content_key in storage_data:
+        try:
+            storage_data.update(content_dict)
+            write_database(storage_data)
+            return create_headers(200, "OK")
+        except Exception as e:
+            return create_headers(500, str(e))
+    else:
+        return create_headers(404, "Not Found")
+
+
+# Processo de solicitação do tipo DELETE
+def do_DELETE(data: list):
+    content = get_content(data)
+    if content == None:
+        return create_headers(400, "Bad Request")
+
+    storage_data = read_database()
+
+    if content in storage_data:
+        storage_data.pop(content)
+        write_database(storage_data)
+        return create_headers(200, "OK")
+    else:
+        return create_headers(404, "Not Found")
+
+
+
+def receive_data(client_socket):
+    client_data = client_socket.recv(MAX_BUFFER_SIZE)
+    decoded_data = (
+        str(client_data).strip("b'").rstrip().replace("\\n", "").replace("\\r", " ").replace("\\t", "")
+    )
+    data_variables = str(decoded_data).split()
+
+    return data_variables
+
+
+if __name__ == "__main__":
+    main()
